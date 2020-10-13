afcce944abba97cf73dbea20829cd5e6b14abf13
IgorKovalenko11 <72105260+IgorKovalenko11@users.noreply.github.com>
2020-10-13 21:41:18 +0300

    Add files via upload

---
+++ лаба5.cpp
@@ -0,0 +1,35 @@
+﻿#include<stdio.h>
+#include<iostream>
+#include<math.h>
+
+
+int main()
+{
+	setlocale(LC_ALL, "ru");
+	double a, b, c;
+	printf("Введите стороны треугольника\n");
+	scanf_s("%lf%lf%lf", &a, &b, &c);
+	if (a <= 0 || b <= 0 || c <= 0)
+	{
+		printf("Такой треугольник не существует!\n");
+
+		return 1;
+	}
+	if (a == b && a == c)
+	{
+		printf("Треугольник равносторонний");
+	}
+	else if (a * a + b * b == c * c || a * a + c * c == b * b || b * b + c * c == a * a)
+	{
+		printf("Треугольник прямоугольный");
+	}
+	else
+	{
+		printf("Треугольник простой");
+	}
+
+
+	return 0;
+
+}
+
