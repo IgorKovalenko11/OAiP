c52f9906898b0d196a3ce11b5574d006cdd050ee
IgorKovalenko11 <72105260+IgorKovalenko11@users.noreply.github.com>
2020-11-10 22:58:47 +0300

    Add files via upload

---
+++ Лаборатоная3.cpp
@@ -0,0 +1,47 @@
+﻿#include<locale.h>
+#include<stdio.h>
+#include<stdlib.h>
+int main() {
+
+	setlocale(LC_ALL, "rus");
+	int* a;
+	int i, n, chp, chn, sp, sn, ch0;
+
+	printf("Введите размер массива: ");
+	scanf_s("%d", &n);
+
+	a = (int*)malloc(n * sizeof(int));
+	printf("Введите элементы массива:\n ");
+	for (i = 0; i < n; i++)
+	{
+		scanf_s("%d", &a[i]);
+	}
+	chp = 0; chn = 0; sp = 0; sn = 0; ch0 = 0;
+
+	for (i = 0; i < n; i++)
+	{
+		if (a[i] > 0)
+		{
+			sp = sp + a[i]; chp++;
+		}
+		else if (a[i] < 0)
+		{
+			sn = sn + a[i]; chn++;
+		}
+		else
+		{
+			ch0++;
+		}
+	}
+	printf("Количество положительных: %d\n", chp);
+	printf("Количество отрицательных: %d\n", chn);
+	printf("Количество равных нулю: %d\n", ch0);
+	printf("Сумма положительных: %d\n", sp);
+	printf("Сумма отрицательных %d", sn);
+	return 0;
+}
+
+
+
+
+
